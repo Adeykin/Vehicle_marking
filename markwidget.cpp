c78c964b5874b9b93bfb4af836fa2282830598c3
Adeykin <adeykin90@gmail.com>
2017-04-02 15:23:36 +0300

    add cars function

---
+++ markwidget.cpp
@@ -0,0 +1,66 @@
+#include "markwidget.h"
+
+#include <QDebug>
+#include <QtGui>
+
+MarkWidget::MarkWidget(QWidget *parent) : QWidget(parent)
+{
+    //resize(500,500);
+}
+
+void MarkWidget::setImage(QImage *image)
+{
+    this->image = image;
+    resize(image->size());
+}
+
+void MarkWidget::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        qDebug() << "add point " << event->pos().x() << ' ' << event->pos().y();
+        points.push_back( event->pos() );
+        if(points.size() == 4)
+        {
+            qDebug() << "finish rect";
+            poligons.push_back(points);
+            points.clear();
+        }
+    }
+    if(event->button() == Qt::RightButton)
+    {
+        qDebug() << "remove detection";
+    }
+    update();
+}
+
+void MarkWidget::mouseMoveEvent(QMouseEvent *event)
+{
+}
+
+void MarkWidget::paintEvent(QPaintEvent *e)
+{
+    qDebug() << "painting MarkWidget";
+
+    //QImage image("/home/adeykin/projects/parking/115000004/901000011/20170327_121030.jpg");
+
+    if(image)
+    {
+        //Draw image
+        QPixmap img = QPixmap::fromImage(*image);
+        QPainter p(this);
+        p.drawPixmap(0,0,img);
+
+        //Draw all poligones
+        p.setPen(QPen(Qt::green, 3, Qt::SolidLine, Qt::RoundCap));
+        for(auto it = poligons.begin(); it != poligons.end(); it++)
+        {
+            p.drawPolygon(*it);
+        }
+
+        //Draw current poligin
+        p.setPen(QPen(Qt::green, 3, Qt::SolidLine, Qt::RoundCap));
+        p.drawPolyline(points);
+    }
+}
+
---
+++ markwidget.h
@@ -0,0 +1,30 @@
+#ifndef MARKWIDGET_H
+#define MARKWIDGET_H
+
+#include <QWidget>
+
+#include <list>
+
+class MarkWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MarkWidget(QWidget *parent = 0);
+
+    void setImage(QImage* image);
+
+    void mousePressEvent(QMouseEvent* event);
+    void mouseMoveEvent(QMouseEvent* event);
+
+protected:
+    void paintEvent(QPaintEvent* e);
+private:
+    QImage* image = NULL;
+    QVector<QPoint> points;
+    QVector<QVector<QPoint> > poligons;
+signals:
+
+public slots:
+};
+
+#endif // MARKWIDGET_H
